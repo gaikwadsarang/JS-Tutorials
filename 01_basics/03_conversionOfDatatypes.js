733b5d71cc005c3febf486e92cb0b5a23294b61e
Sarang Gaikwad <128568827+gaikwadsarang@users.noreply.github.com>
2024-08-30 13:18:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOfDatatypes.js
@@ -0,0 +1,30 @@
+let score = "sarang"
+
+// console.log(typeof score);
+
+let valueInNUmber = Number(score)
+// console.log(typeof valueInNUmber);
+// console.log(valueInNUmber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+
+let isLoggedIn = "sarang"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "sarang" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
