51880a0d97733d4953c474efbb60c68c7f301622
Sarang Gaikwad <128568827+gaikwadsarang@users.noreply.github.com>
2024-08-30 06:58:18 +0000

    dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we are using node.js , not browser
+
+console.log(3 + 3);
+// code readability should high
+
+let name = "sarang"
+let age = 13
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt 
+// string => ""/ ''
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
+
+
+
