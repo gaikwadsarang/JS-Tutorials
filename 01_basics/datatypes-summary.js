72b4dea56d491808ebd6dedacf8a4add12416688
Sarang Gaikwad <128568827+gaikwadsarang@users.noreply.github.com>
2024-08-30 14:20:19 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, bigInt, symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+
+const bigNumber = 121334354365654546567554n
+
+// Reference (Non-primitive)
+
+//Arrays, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"]
+let Obj = {
+    name: "sarang",
+    age: 22
+}
+
+const myFunction= function() {
+    console.log("hello World");
+}
+
+console.log(typeof heroes);
+
