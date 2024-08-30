13f26854f68b004a5aea6f7e8bee7fbc73d974ba
Sarang Gaikwad <128568827+gaikwadsarang@users.noreply.github.com>
2024-08-30 13:54:02 +0000

    done

--- 01_basics/03_conversionOfDatatypes.js
+++ 01_basics/03_conversionOfDatatypes.js
@@ -25,6 +25,44 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// OPERATIONS
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " sarang"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1"+2+ 2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
 
