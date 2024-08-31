d20edde481f4a07a12f1098259d0716f062bca4a
Sarang Gaikwad <128568827+gaikwadsarang@users.noreply.github.com>
2024-08-31 13:54:00 +0000

    done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -27,5 +27,33 @@ const myFunction= function() {
     console.log("hello World");
 }
 
-console.log(typeof heroes);
+// console.log(typeof heroes);
+
+// Memory
+// Stack(Primitive), Heap(non-primitive)
+
+let myYoutubename = "sarangGaikwad"
+let anotherName = myYoutubename
+anotherName = "djBravo"
+
+// console.log(myYoutubename);
+
+// console.log(anotherName);
+
+let userOne = {
+    email: "123@gmail.com",
+    upi: "userh@ybl"
+}
+
+let usertwo = userOne
+
+usertwo.email = "user@gmail.com"
+
+console.log(userOne.email);
+console.log(usertwo.email);
+
+
+
+
+
 
