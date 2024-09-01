11bc9186cb5a304503b2bfed513b5d32c07f419e
Sarang Gaikwad <128568827+gaikwadsarang@users.noreply.github.com>
2024-09-01 12:52:56 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+const name = "sarang"
+const repoCount = 50
+
+// console.log(name + repoCount + "value");
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('sarangG-s')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString =  gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newString1 = "        sarang     "
+// console.log(newString1.trim());
+
+const url = "https://sarang.com/sarang%20G"
+// console.log(url.replace("%20", "-"))
+
+console.log(url.includes("sarang"));
+
+console.log(gameName.split("-"));
+
+
+
+
