80dbd2af8e2e7cf145710249a180235890c0bbbd
Sarang Gaikwad <128568827+gaikwadsarang@users.noreply.github.com>
2024-10-02 08:45:36 +0000

    oop

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -58,7 +58,7 @@ console.log(username);
 
 const promiseFive = new Promise((resolve, reject) => {
     setTimeout(() => {
-        let error = false
+        let error = true
         if(!error) {
             resolve({username: "javascript", passwaord: "123"})
         }else {
@@ -68,7 +68,35 @@ const promiseFive = new Promise((resolve, reject) => {
 });
 
 async function consumePromiseFive(){
-const resopnse = await promiseFive
-console.log(resopnse);
-
+try {
+    const resopnse = await promiseFive
+    console.log(resopnse);
+    
+} catch (error) {
+    console.log(error);
 }
+}
+
+consumePromiseFive();
+
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//         // console.log(response);
+//     } catch (error) {
+//         console.log("E:", error);
+//     }
+// }
+
+// getAllUsers();
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((resopnse) => {
+    return resopnse.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,23 @@
+# javascript & classes
+
+## OOP 
+
+## Object 
+- collection of propertied and methods.
+- toLoweCase
+
+## why use OOP
+
+## parts of OOP
+Object literal {}
+
+- Constructor function
+- Prototypes
+- Classes
+-Instances ( new, this)
+
+## 4 pillars
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
